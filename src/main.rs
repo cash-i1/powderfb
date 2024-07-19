5955e8ef3c8111925ddbb8c5f1f3b76b80e560e2
cash-i1 <cashignitro@gmail.com>
2024-07-20 08:37:18 +1000

    switched to builder system

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,8 @@ struct Graphics {
 }
 
 impl Graphics {
-    fn new(width: usize, height: usize) -> Self {
-        let buffer = vec![0u32; width * height];
-
-        let window = Window::new(
-            "window",
-            width,
-            height,
-            WindowOptions {
-                ..WindowOptions::default()
-            },
-        )
-        .expect("Unable to create the window");
-
-        Self {
-            buffer,
-            dimensions: WindowDimensions {width, height},
-            window,
-        }
+    fn builder() -> GraphicsBuilder {
+        GraphicsBuilder::new()
     }
     fn rectangle(&mut self, (x, y): (usize, usize), (width, height): (usize, usize)) {
         for i in 0..width {
@@ -51,9 +35,72 @@ impl Graphics {
     }
 }
 
+struct GraphicsBuilder {
+    buffer: Option<Vec<u32>>,
+    dimensions: Option<WindowDimensions>,
+    fps: Option<usize>
+}
+impl GraphicsBuilder {
+    pub fn new() -> Self {
+        Self {
+            buffer: None,
+            dimensions: None,
+            fps: None
+        }
+    }
+    pub fn width(&mut self, width: usize) -> &mut Self {
+        if let Some(dims) = &mut self.dimensions {
+            dims.width = width;
+        } else {
+            self.dimensions = Some(WindowDimensions { width, height: 0 })
+        }
+        self
+    }
+    pub fn height(&mut self, height: usize) -> &mut Self {
+        if let Some(dims) = &mut self.dimensions {
+            dims.height = height;
+        } else {
+            self.dimensions = Some(WindowDimensions { width: 0, height })
+        }
+        self
+    }
+    pub fn fps(&mut self, fps: usize) -> &mut Self {
+        self.fps = Some(fps);
+        self
+    }
+    pub fn build(&mut self) -> Graphics {
+        let buffer = self.buffer.take().unwrap();
+        let dimensions = self.dimensions.take().unwrap();
+
+        let mut window = Window::new(
+            "window",
+            dimensions.width,
+            dimensions.height,
+            WindowOptions {
+                ..WindowOptions::default()
+            },
+        )
+        .expect("unable to create the window");
+
+        if let Some(fps) = self.fps {
+            window.set_target_fps(fps);
+        }
+
+        Graphics {
+            buffer,
+            dimensions,
+            window,
+        }
+    }
+}
+
 fn main() {
-    let mut gfx = Graphics::new(1280, 720);
-    gfx.window.set_target_fps(60);
+    let mut gfx = Graphics::builder()
+        .width(1280)
+        .height(720)
+        .fps(60)
+        .build();
+
     while gfx.window.is_open() {
         gfx.rectangle((100, 100), (20, 20));
         gfx.pixel((10, 10));
