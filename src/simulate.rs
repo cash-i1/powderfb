04f7960e6e2d906fe23e6437f0694585abb23408
cash-i1 <cashignitro@gmail.com>
2024-07-21 18:48:56 +1000

    implemented other physics

--- src/simulate.rs
+++ src/simulate.rs
@@ -7,6 +7,8 @@ impl Simulate {
             match particle.properties.derives {
                 ParticleType::Basic => Self::basic(world, (i, j)),
                 ParticleType::Sand => Self::sand(world, (i, j)),
+                ParticleType::Still => {}
+                ParticleType::Water => Self::water(world, (i, j)),
                 _ => Self::basic(world, (i, j)),
             }
         }
@@ -36,6 +38,31 @@ impl Simulate {
                 && world.particles[i + 1][j + 1].is_none()
             {
                 world.particles[i + 1][j + 1] = Some(particle);
+            } else {
+                world.particles[i][j] = Some(particle);
+            }
+        } else {
+            world.particles[i][j] = None;
+        }
+    }
+    pub fn water(world: &mut World, (i, j): (usize, usize)) {
+        if let Some(particle) = world.particles[i][j].take() {
+            if j + 1 < world.particles[i].len() && world.particles[i][j + 1].is_none() {
+                world.particles[i][j + 1] = Some(particle);
+            } else if i.checked_sub(1).unwrap_or(0) < world.particles.len()
+                && j + 1 < world.particles[i].len()
+                && world.particles[i.checked_sub(1).unwrap_or(0)][j + 1].is_none()
+            {
+                world.particles[i.checked_sub(1).unwrap_or(0)][j + 1] = Some(particle);
+            } else if i + 1 < world.particles.len()
+                && j + 1 < world.particles[i].len()
+                && world.particles[i + 1][j + 1].is_none()
+            {
+                world.particles[i + 1][j + 1] = Some(particle);
+            } else if i + 1 < world.particles.len() && world.particles[i - 1][j].is_none() {
+                world.particles[i - 1][j] = Some(particle);
+            } else if i + 1 < world.particles.len() && world.particles[i + 1][j].is_none() {
+                world.particles[i + 1][j] = Some(particle);
             } else {
                 world.particles[i][j] = Some(particle);
             }
