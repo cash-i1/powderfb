8219db251b98d16499d31ee770861df34b4fb087
cash-i1 <cashignitro@gmail.com>
2024-07-23 19:02:02 +1000

    ui: drawing now with new system

--- src/ui/components/button.rs
+++ src/ui/components/button.rs
@@ -1,14 +1,56 @@
 use crate::misc::Color;
 use crate::misc::Rectangle;
+use crate::ui::UiObject;
+use crate::graphics::Graphics;
+use crate::particle::particles;
+use crate::particle::Particle;
+use crate::World;
 
-pub struct Button {
-    rect: Rectangle,
-    focused: bool,
-    focused_color: Color,
-    default_color: Color,
-    current_color: Color,
-    id: String,
-    toggled: bool,
+pub struct ChooseParticleButton {
+    pub rect: Rectangle,
+    pub focused: bool,
+    pub focused_color: Color,
+    pub default_color: Color,
+    pub toggled: bool,
 }
 
+impl ChooseParticleButton {
+    pub fn objects() -> Vec<Box<dyn UiObject>> {
+        let mut buttons = vec![];
+        for (i, particle) in particles().iter().enumerate() {
+            let btn = ChooseParticleButton {
+                rect: Rectangle {
+                    height: 35,
+                    width: 35,
+                    x: 10 + 50 * i,
+                    y: 10,
+                },
+                focused: false,
+                focused_color: Color::Custom((particle.color.raw() & 0xfefefe) >> 1),
+                default_color: particle.color,
+                toggled: false,
+            };
+            buttons.push(Box::new(btn) as Box<dyn UiObject>);
+        }
+        buttons
+    }
+}
 
+impl UiObject for ChooseParticleButton {
+    fn rect(&self) -> Rectangle {
+        self.rect.clone()
+    }
+
+    fn color(&self) -> Color {
+        if self.focused {
+            self.focused_color.clone()
+        } else {
+            self.default_color.clone()
+        }
+    }
+
+    fn init(&mut self) {
+        todo!()
+    }
+
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -6,24 +6,28 @@ use crate::World;
 pub mod components;
 
 pub trait UiObject {
-    fn init(&mut self) -> Rectangle;
+    fn init(&mut self);
     fn rect(&self) -> Rectangle;
     fn color(&self) -> Color;
-    fn render(&self, gfx: &Graphics);
 }
 
 pub struct Ui {
     pub focused: bool,
-    pub buttons: Option<Vec<Box<dyn UiObject>>>,
+    pub objects: Vec<Box<dyn UiObject>>,
 }
 impl Ui {
     pub fn new() -> Self {
         Self {
             focused: false,
-            buttons: None,
+            objects: vec![],
         }
     }
-    pub fn init(&mut self, gfx: &Graphics) {}
+    pub fn init(&mut self, gfx: &Graphics) {
+    }
     pub fn step(&mut self, gfx: &mut Graphics, world: &mut World) {}
-    pub fn draw(&mut self, gfx: &mut Graphics) {}
+    pub fn draw(&mut self, gfx: &mut Graphics) {
+        for obj in &mut components::button::ChooseParticleButton::objects() {
+            gfx.rectangle(obj.rect(), obj.color());
+        }
+    }
 }
